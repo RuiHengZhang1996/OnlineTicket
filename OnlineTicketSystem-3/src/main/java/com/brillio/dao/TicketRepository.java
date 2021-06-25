044fd83f69ecd139bbd520d365ff43d8eb53d1b1
Fareed Khan <34606876+FareedKhanGithub@users.noreply.github.com>
2021-06-24 23:27:46 -0500

    Delete OnlineTicketSystem-3 directory

--- OnlineTicketSystem-3/src/main/java/com/brillio/dao/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brillio.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.brillio.entity.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-
-}
